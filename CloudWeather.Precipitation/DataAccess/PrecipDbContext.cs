85bb7ddd4890717241e794f96c60133c0905c36a
alex <alex@MBPsomtlhoralex.localdomain>
2022-08-24 14:03:33 +0200

    add data access to precipitation service

---
+++ CloudWeather.Precipitation/DataAccess/PrecipDbContext.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace CloudWeather.Precipitation.DataAccess
+{
+    public class PrecipDbContext : DbContext
+    {
+        public PrecipDbContext() { }
+
+        public PrecipDbContext(DbContextOptions opts) : base(opts) { }
+
+        public DbSet<Precipitation> Precipitation { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            SnakeCaseIdentityTableName(modelBuilder);
+        }
+
+        private static void SnakeCaseIdentityTableName(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Precipitation>(b => { b.ToTable("precipitation"); });
+        }
+    }
+}
+
+    
--- CloudWeather.Precipitation/Program.cs
+++ CloudWeather.Precipitation/Program.cs
@@ -1,5 +1,33 @@
+using System.Linq;
+using CloudWeather.Precipitation.DataAccess;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
+
+builder.Services.AddDbContext<PrecipDbContext>(
+    opts =>
+    {
+        opts.EnableSensitiveDataLogging();
+        opts.EnableDetailedErrors();
+        opts.UseNpgsql(builder.Configuration.GetConnectionString("AppDb"));
+    }, ServiceLifetime.Transient
+);
+
 var app = builder.Build();
+
+app.MapGet("/observation/{zip}", (string zip, [FromQuery] int? days, PrecipDbContext db) => {
+    if(days == null || days < 1 || days > 30)
+    {
+        return Results.BadRequest("Please provide a 'days' query param beteween 1 and 30'"); 
+    }
+    var startDate = DateTime.UtcNow - TimeSpan.FromDays(days.Value);
+    var results = db.Precipitation
+    .Where(precip => precip.ZipCode == zip && precip.CreatedOn > startDate)
+    .ToListAsync();
+
+    return Results.Ok(results);
+});
 
 app.Run();
